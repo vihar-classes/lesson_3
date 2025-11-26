88ffd0b7c25431d92cf357e8321a8bac0403b1eb
Vihar <vihar_classes@proton.me>
2025-11-26 19:44:26 +0800

    Update sentence.py

--- sentence.py
+++ sentence.py
@@ -9,3 +9,15 @@ for word in words_array:
 final_output = " ".join(reversed_words)
 print(f"original: {words_array}")
 print(f"reversed: {final_output}")
+
+longest_palindrome = ""
+
+for word in words_array:
+    if word == word[::-1]:
+        if len(word) > len(longest_palindrome):
+            longest_palindrome = word
+        
+if longest_palindrome:
+    print(f"longest palindrome: '{longest_palindrome}' (Length: {len(longest_palindrome)})")
+else:
+    print("no palindromic word found.")
