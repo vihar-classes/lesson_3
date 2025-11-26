efc5584eed1d40a96dcfa6b9dd2a8a02bce98987
Vihar <vihar_classes@proton.me>
2025-11-26 19:24:22 +0800

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,9 @@
+var1 = "hello"
+var2 = 5
+var3 = 10.5
+var4 = "vihar"
+
+print(f"var1 is: {type(var1)}")
+print(f"var2 is: {type(var2)}")
+print(f"var3 is: {type(var3)}")
+print(f"var5 is: {type(var4)}")
